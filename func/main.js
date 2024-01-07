17941a4f5ab26632728d7d83f16e3d8b06a256ab
Rifqi Munawar <rifqimunawar48@gmail.com>
2024-01-07 23:57:13 +0700

    function argurmnt

---
+++ func/main.js
@@ -0,0 +1,16 @@
+// let lembarDadu = Math.floor(Math.random()*6)+1;
+// console.log(lembarDadu)
+
+
+// Argument dan parameter dalam function  
+// =====================================
+// =====================================
+function selamatPagi(name){
+  console.log(`Halo, ${name}. selamat pagi`)
+}
+selamatPagi ('Rifqi Munawar')
+
+function selamatMalam(namaAnda){
+  console.log(`selamat malam  ${namaAnda}. apakah anda belum tidur?`)
+}
+selamatMalam('anjay nama saya rifqi')
