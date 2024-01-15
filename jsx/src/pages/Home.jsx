8f62a43ed6ca8e5adc2905e89e8e153418390c65
Rifqi Munawar <rifqimunawar48@gmail.com>
2024-01-15 16:14:31 +0700

    hero section

--- jsx/src/pages/Home.jsx
+++ jsx/src/pages/Home.jsx
@@ -4,15 +4,34 @@ import FooterComponent from '../components/FooterComponent'
 
 export default function Home() {
   return (
-    <section>
+    <>
       <Helmet>
         <title>Home Anjir</title>
       </Helmet>
       <NavbarComponent />
-      <section>
-        
+      <section id="hero" className="d-flex align-items-center">
+        <div className="container">
+          <h1>
+            Salam <span>Pergerakan</span>
+          </h1>
+          <h2>
+            We are team of talented designers making websites with Bootstrap
+          </h2>
+          <div className="d-flex">
+            <a href="#about" className="btn-get-started scrollto">
+              Get Started
+            </a>
+            <a
+              href="https://www.youtube.com/watch?v=jDDaplaOz7Q"
+              className="glightbox btn-watch-video"
+            >
+              <i className="bi bi-play-circle"></i>
+              <span>Watch Video</span>
+            </a>
+          </div>
+        </div>
       </section>
-      <FooterComponent/>
-    </section>
+      <FooterComponent />
+    </>
   )
 }
